d782263cbf97972da40f7c7a8ea67274a3b27ba2
Semyon <s.golokolos@school.nsu.ru>
2024-10-21 10:12:51 +0700

    Сделал до 5 не вкл.

--- task3.py
+++ task3.py
@@ -7,3 +7,18 @@
 # [1, 2, 3, 2, 5, 10]
 # Вывод:
 # значение:2 индексы 1 и 3
+from random import randint
+print('Enter length of massive:', end=' ')
+n = int(input())
+b = False
+if n > 0:
+    M = [randint(0, 100)
+         for i in range(n)]
+    for i in range(0, n - 1):
+        for o in range(i + 1, n):
+            if M[i] == M[o]:
+                print('Value of equal elements:', M[i], 'Indexes:', i, 'and', o)
+                b = True
+    if not(b):
+        print("Нет")
+    print(M)
--- task5.py
+++ task5.py
@@ -1,2 +1,19 @@
 # Введите массив длины N с клавиатуры и найдите (за один проход)
 # количество элементов, имеющих максимальное значение.
+from random import randint
+print('Enter length of massive:', end=' ')
+n = int(input())
+s = 0
+if n > 0:
+    M = [randint(0, 100)
+        for i in range(0, n)]
+    print(M)
+    for i in range(1, n):
+        if M[i-1] < M[i]:
+            m = M[i]
+        else:
+            m = M[0]
+    for i in range(0, n):
+        if M[i] == m:
+            s +=1
+    print(s)
